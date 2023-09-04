eb393ae2de4b640f31f7ad384d8287c186f70c97
Barcha1 <abbarcha123456@gmail.com>
2023-09-05 03:09:21 +0500

    ss

--- src/pages/createMeal/CreateMeal.jsx
+++ src/pages/createMeal/CreateMeal.jsx
@@ -435,7 +435,7 @@ const CreateMeal = () => {
                   </Box>
                 </Box>
               </Grid>
-              <Grid item lg={4}>
+              <Grid item lg={4} md={4} sm={12} xs={12}>
                 <InputLabel
                   sx={{
                     color: " #888587",
@@ -523,7 +523,7 @@ const CreateMeal = () => {
                   </Box>
                 </Box>
               </Grid>
-              <Grid item lg={4}>
+              <Grid item lg={4} md={4} sm={12} xs={12}>
                 <InputLabel
                   sx={{
                     color: " #888587",
